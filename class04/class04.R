daa916c43cc5024fd83b1a286eab5a906aa75052
Aadhya Tripathi <aatripathi@ucsd.edu>
2026-03-13 11:40:03 -0700

    Added class 04 files

---
+++ class04/class04.R
@@ -0,0 +1,9 @@
+#' ---
+#' title: "Class 4 Intro to R"
+#' author: "Aadhya Tripathi"
+#' ---
+
+x <- 1:50
+plot(x)
+plot(x, sin(x), type = "l", col = "green", lwd = 3,
+     xlab = "Is it lunch time yet?")
